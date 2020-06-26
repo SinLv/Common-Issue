0aa4f1930597227704bd783cc770f072f0e30523
Lv Xi <lvxi1014@outlook.com>
2020-06-26 09:11:32 +0800

    删除文件

--- Nodejs/electron/client/src/reducer.js
+++
@@ -1,8 +0,0 @@
-import {combineReducers} from 'redux'
-import { user } from './redux/user.redux'
-import { chatuser } from './redux/chatuser.redux'
-import {chat} from './redux/chat.redux'
-
-//此方法用来合并多个reducer，可以同时传递多个store
-
-export default combineReducers({user,chatuser,chat})
--- webpack/《深入浅出webpack例子》/5-3编写Loader/loaders/comment-require-loader.js
+++
@@ -1,8 +0,0 @@
-function replace(source) {
-    // 使用正则把 // @require './main.css' 转换成 require('./main.css');  
-    return source.replace(/(\/\/ *@require)/, 'require');
-}
-
-module.exports = function (content) {
-    return replace(content);
-};
